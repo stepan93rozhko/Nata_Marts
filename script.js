c2b9d1c223508e0316a855fb4119aff2b8621d22
Stepan <stepan93rozhko@gmail.com>
2020-04-11 16:46:56 +0300

    заміна шрифтів

--- script.js
+++ script.js
@@ -6,6 +6,15 @@ $(document).ready(function(){
         $(".footer").toggle();
     });
     
-   
+
+    //800 -длительность скроллинга в мс---------------------------------------------------
+    $(function(){
+        $('a[href^="#"]').click(function(){
+        var target = $(this).attr('href');
+        $('html, body').animate({scrollTop: $(target).offset().top}, 800);
+        return false;
+        });
+    });
+    //--------------------------------------------------------------------------------
 
 });
